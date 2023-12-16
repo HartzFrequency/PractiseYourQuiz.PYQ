3b2c48fa4a72997fa296dca9e4dd1de882e1f4f9
Harsh Shrivastava <harshshrivastava554@gmail.com>
2023-12-16 13:25:52 +0530

    added the result mechanism and data optimisation

--- Quiz.py
+++ Quiz.py
@@ -7,19 +7,24 @@ import json
 with open("DataScience\\QuizDataBase.json", "r") as file:
     quiz_data = json.load(file)
 
+# Create a text file to log incorrect answers
+log_file = open("incorrect_answers.txt", "w")
+
 # Function to display the current question and choices
 def show_question():
     # Get the current question from the quiz_data list
-    question = quiz_data[current_question]
-    qs_label.config(text=question["question"])
+    question_data = quiz_data[current_question]
+    qs_label.config(text=question_data["question"])
 
     # Display the choices on the buttons
-    choices = question["options"]
+    choices = question_data["options"]
     for i in range(4):
-        choice_btns[i].config(text=choices[i], state="normal")  # Reset button state
+        # Reset button state and update wrap length for each button
+        choice_btns[i].config(text=choices[i], state="normal", wraplength=200, width=30, height=3)  # Set your desired size
 
-    # Clear the feedback label and disable the next button
+    # Clear the feedback label, correct option label, and disable the next button
     feedback_label.config(text="")
+    correct_option_label.config(text="")
     next_btn.config(state="disabled")
 
     # Update the canvas scroll region
@@ -29,14 +34,14 @@ def show_question():
 # Function to check the selected answer and provide feedback
 def check_answer(choice):
     # Get the current question from the quiz_data list
-    question = quiz_data[current_question]
+    question_data = quiz_data[current_question]
     selected_choice = choice_btns[choice].cget("text")
 
     # Extract the choice identifier (e.g., "a", "b", "c", "d") from the selected choice
     selected_identifier = selected_choice.split(".")[0].strip().lower()
 
     # Check if the selected choice identifier matches the correct answer
-    if selected_identifier == question["correct_option"]:
+    if selected_identifier == question_data["correct_option"]:
         # Update the score and display it
         global score
         score += 1
@@ -44,7 +49,12 @@ def check_answer(choice):
         feedback_label.config(text="Correct!", foreground="green")
     else:
         feedback_label.config(text="Incorrect!", foreground="red")
-    
+        # Show the correct option in green below the incorrect text
+        correct_option_label.config(text="Correct answer: {}".format(question_data["correct_option"]), foreground="green")
+
+        # Log the incorrect answer to the text file
+        log_file.write("Quiz {}: Question {}: Incorrect\n".format(question_data["quiz_no"], question_data["question_no"]))
+
     # Disable all choice buttons and enable the next button
     for button in choice_btns:
         button.config(state="disabled")
@@ -104,12 +114,16 @@ qs_label = ttk.Label(
 )
 qs_label.pack(pady=10)
 
-# Create the choice buttons
+# Create the choice buttons using tk.Button
 choice_btns = []
 for i in range(4):
-    button = ttk.Button(
+    button = tk.Button(
         frame,
-        command=lambda i=i: check_answer(i)
+        command=lambda i=i: check_answer(i),
+        wraplength=200,  # Set your desired wraplength
+        justify="left",  # Align text to the left
+        width=30,  # Set your desired width
+        height=3  # Set your desired height
     )
     button.pack(pady=5)
     choice_btns.append(button)
@@ -121,6 +135,14 @@ feedback_label = ttk.Label(
     padding=10
 )
 feedback_label.pack(pady=10)
+
+# Create a label to display the correct option when the answer is incorrect
+correct_option_label = ttk.Label(
+    frame,
+    anchor="center",
+    padding=10
+)
+correct_option_label.pack(pady=10)
 
 # Initialize the score
 score = 0
@@ -155,3 +177,6 @@ canvas.configure(scrollregion=canvas.bbox("all"))
 
 # Start the main event loop
 root.mainloop()
+
+# Close the log file
+log_file.close()
